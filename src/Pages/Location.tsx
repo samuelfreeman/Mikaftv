a7dd130e3f14f8d39a485e9e5ce4d2def0291547
samuelfreeman <samuelmanueljnr@gmail.com>
2025-02-10 18:13:05 +0000

    resized the location

--- src/Pages/Location.tsx
+++ src/Pages/Location.tsx
@@ -3,7 +3,7 @@
 const Location = () => {
   return (
     
-    <div className="w-full flex h-[80vh]  md:mb-40 md:mt-0 p-6 mt-56 justify-center flex-col items-center">
+    <div className="w-full flex min-h-[120vh] md:max-h-[80vh] md:mb-40 md:mt-0 p-6 mt-56 justify-center flex-col items-center">
         <h1 className="text-center font-bold text-4xl pb-8">Our Location:</h1>
     <iframe
       src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3962.7637320335625!2d-1.6450646257967318!3d6.676167893319007!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xfdb97dceac8945b%3A0x4524b5d59fe4f2da!2sMikaf%20Multimedia!5e0!3m2!1sen!2sgh!4v1739208581528!5m2!1sen!2sgh"
